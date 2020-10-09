29902089a0da6839fa9672e2f116a1d74982b68e
Wang, Yang <yang3.wang@intel.com>
2020-10-09 07:13:17 -0400

    update run.sh

--- models/image_recognition/tensorflow/resnet50v1_5/training/mlperf_resnet/run.sh
+++ models/image_recognition/tensorflow/resnet50v1_5/training/mlperf_resnet/run.sh
@@ -1,18 +1,11 @@
-RANDOM_SEED=`date +%s`
-QUALITY=0.759
-set -e
-
 # Register the model as a source root
 export PYTHONPATH="$(pwd):${PYTHONPATH}"
-
 export KMP_BLOCKTIME=0
 
-# 4 instances
+# 8 instances
 export OMP_NUM_THREADS=6
-export KMP_AFFINITY=disable
+export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_SETTINGS=1
-
-export ANALYTICS_ZOO_HOME=/home/cpx/yang/dist/
-export SPARK_HOME=/opt/spark-2.4.3-bin-hadoop2.7
-bash $ANALYTICS_ZOO_HOME/bin/spark-submit-python-with-zoo.sh --master spark://cpx-1:7077 --executor-cores 1 --total-executor-cores 16 --driver-memory 20g --executor-memory 45g  imagenet_main.py 1 --model_dir ./logs --batch_size 128 --version 1 --resnet_size 50 --use_bfloat16 --use_synthetic_data
-
+export ANALYTICS_ZOO_HOME=/opt/work/analytics-zoo/dist/
+export SPARK_HOME=/opt/work/spark-2.4.3
+bash $ANALYTICS_ZOO_HOME/bin/spark-submit-python-with-zoo.sh --master spark://$(hostname):7077 --executor-cores 1 --total-executor-cores 8 --driver-memory 20g --executor-memory 18g --conf spark.network.timeout=10000000 --conf spark.executor.heartbeatInterval=100000 imagenet_main.py 1 --model_dir ./logs --batch_size 128 --version 1 --resnet_size 50 --train_epochs 90 --data_dir /opt/ILSVRC2012/ --use_bfloat16
